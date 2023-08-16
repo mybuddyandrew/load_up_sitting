408fdc5f896afd0ff267a0c7c6caf1276bbde660
Andrew Hodson <andrewlloyd.hodson@gmail.com>
2023-08-16 13:25:26 -0400

    Make it look nice

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,6 +2,9 @@ import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
 
+import { Slideover } from "tailwindcss-stimulus-components"
+application.register('slideover', Slideover)
+
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
