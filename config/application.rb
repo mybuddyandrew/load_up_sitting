d9f2daedece23935dc9a99ef92c2d75737f00b98
Andrew Hodson <andrewlloyd.hodson@gmail.com>
2023-08-16 11:49:54 -0400

    Load Engine

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module LoadUpSitting
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.autoload_paths += %W(#{config.root}/app/engines)
   end
 end
