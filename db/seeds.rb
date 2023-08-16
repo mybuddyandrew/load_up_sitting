8f27a29ebcd927343d4add74fded7f309fe6b1b1
Andrew Hodson <andrewlloyd.hodson@gmail.com>
2023-08-16 13:33:12 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+booking_data = [
+  { first_name: "Alice", last_name: "Johnson", animal_name: "Buddy", animal_type: "Dog", hours_requested: 4, date_of_service: Date.today + 5.days },
+  { first_name: "Bob", last_name: "Smith", animal_name: "Kitty", animal_type: "Cat", hours_requested: 3, date_of_service: Date.today + 7.days },
+  { first_name: "Charlie", last_name: "Brown", animal_name: "Charlie", animal_type: "Dog", hours_requested: 2, date_of_service: Date.today + 10.days },
+  { first_name: "David", last_name: "Williams", animal_name: "Max", animal_type: "Dog", hours_requested: 6, date_of_service: Date.today + 3.days }
+]
+
+booking_data.each do |data|
+  Booking.create!(data)
+end
+
+puts "Seeded #{Booking.count} bookings."
